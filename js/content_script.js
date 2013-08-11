a36108450f4c9884d324723e38d9a05940ddae32
Michael Lumish <michael.lumish@gmail.com>
2013-08-11 16:27:09 -0400

    Added support for more variations of his speech

--- js/content_script.js
+++ js/content_script.js
@@ -3,10 +3,16 @@
 
   function runScript(){
     var list = document.querySelectorAll('.spoiler span[style*="color: #FFFFFF"]');
+    var list2 = document.querySelectorAll('.spoiler span[style*="color: #ffffff"]');
     for(var i=0; i<list.length; i++){
       list[i].style.background = "transparent";
       list[i].onmouseover = function(){this.style.background="green";};
       list[i].onmouseout = function(){this.style.background="transparent";};
+    }
+    for(var i=0; i<list2.length; i++){
+      list2[i].style.background = "transparent";
+      list2[i].onmouseover = function(){this.style.background="green";};
+      list2[i].onmouseout = function(){this.style.background="transparent";};
     }
   }
 
